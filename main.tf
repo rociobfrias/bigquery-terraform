27afe5f07c708c00307842d2c762cf48db0b6e57
rociobfrias <108030459+rociobfrias@users.noreply.github.com>
2022-08-04 14:37:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,7 @@ output "dev_pocdataset_dataset_id" {
 # US_postal_code table
 resource "google_bigquery_table" "US_postal_code" {
   dataset_id = google_bigquery_dataset.dev_pocdataset.dataset_id
-  table_id = var.US_postal_code
-    
+  table_id = var.US_postal_code    
 
   time_partitioning {
     type = "DAY"
@@ -31,15 +30,101 @@ resource "google_bigquery_table" "US_postal_code" {
   schema = <<EOF
 [
   {
-    "name": "permalink",
+    "name": "zip",
+    "type": "INTEGER",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "lat",
+    "type": "FLOAT",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "lng",
+    "type": "FLOAT",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "city",
     "type": "STRING",
     "mode": "NULLABLE",
   },
   {
-    "name": "state",
+    "name": "state_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "state_name",
+    "type": "STRING",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "zcta",
+    "type": "BOOLEAN",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "parent_zcta",
+    "type": "STRING",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "population",
+    "type": "INTEGER",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "density",
+    "type": "FLOAT",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "county_fips",
+    "type": "INTEGER",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "county_name",
+    "type": "STRING",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "county_weights",
+    "type": "STRING",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "county_names_all",
+    "type": "STRING",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "county_fips_all",
+    "type": "STRING",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "imprecise",
+    "type": "BOOLEAN",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "military",
+    "type": "BOOLEAN",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "timezone",
+    "type": "STRING",
+    "mode": "NULLABLE",
+  },
+  {
+    "name": "zip_code",
     "type": "STRING",
     "mode": "NULLABLE",
   }
+  
 ]
 EOF
 }
