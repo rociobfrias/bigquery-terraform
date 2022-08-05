22918457dd94108d9e41e50da0d3d3a5bacc227c
rociobfrias <108030459+rociobfrias@users.noreply.github.com>
2022-08-05 08:55:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,97 +32,97 @@ resource "google_bigquery_table" "US_postal_code" {
   {
     "name": "zip",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "lat",
     "type": "FLOAT",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "lng",
     "type": "FLOAT",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "city",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "state_id",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "state_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "zcta",
     "type": "BOOLEAN",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "parent_zcta",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "population",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "density",
     "type": "FLOAT",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "county_fips",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "county_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "county_weights",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "county_names_all",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "county_fips_all",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "imprecise",
     "type": "BOOLEAN",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "military",
     "type": "BOOLEAN",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "timezone",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "zip_code",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   }
   
 ]
@@ -142,127 +142,127 @@ resource "google_bigquery_table" "articles" {
   {
     "name": "article_id",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "product_code",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "prod_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "product_type_no",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "product_type_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "product_group_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "graphical_appearance_no",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "graphical_appearance_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "colour_group_code",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "colour_group_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "perceived_colour_value_id",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "perceived_colour_value_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "perceived_colour_master_id",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "perceived_colour_master_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "department_no",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "department_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "index_code",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "index_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "index_group_no",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "index_group_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "section_no",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "section_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "garment_group_no",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "garment_group_name",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "detail_desc",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   }
 ]
 EOF
@@ -281,37 +281,37 @@ resource "google_bigquery_table" "customers" {
   {
     "name": "customer_id",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "FN",
     "type": "FLOAT",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "Active",
     "type": "FLOAT",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "club_member_status",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "fashion_news_frequency",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "age",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "postal_code",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   }
 ]
 EOF
@@ -330,27 +330,27 @@ resource "google_bigquery_table" "transactions_train" {
   {
     "name": "t_dat",
     "type": DATE",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "customer_id",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "article_id",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "price",
     "type": "FLOAT",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "sales_channel_id",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   }
 ]
 EOF
@@ -369,12 +369,12 @@ resource "google_bigquery_table" "zip_code" {
   {
     "name": "zip",
     "type": "INTEGER",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   },
   {
     "name": "zip_code",
     "type": "STRING",
-    "mode": "NULLABLE",
+    "mode": "NULLABLE"
   }
 ]
 EOF
