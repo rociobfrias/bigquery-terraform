5b83ad7b2f85afc0843f71f2f4616c541af14bfb
rociobfrias <108030459+rociobfrias@users.noreply.github.com>
2022-08-09 10:25:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "google_bigquery_dataset" "dev_pocdataset" {
   dataset_id                  = var.dev_pocdataset
   friendly_name               = "dev_pocdataset"
   location                    = var.dev_pocdataset_DS_location #check the location
- 
+  deletion_protection         = false
   #default_table_expiration_ms = 3600000
 
 }
