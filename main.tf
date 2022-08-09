fe4c84cb4417642333b68831f887999a0313acd5
alipendix <77787838+alipendix@users.noreply.github.com>
2022-08-09 12:08:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "google_bigquery_dataset" "dev_pocdataset" {
   dataset_id                  = var.dev_pocdataset
   friendly_name               = "dev_pocdataset"
   location                    = var.dev_pocdataset_DS_location #check the location
-  delete_contents_on_destroy  = false
+  delete_contents_on_destroy  = true
   #default_table_expiration_ms = 3600000
 }
 
